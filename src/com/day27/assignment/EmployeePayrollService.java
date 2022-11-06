cbc324864df88868b4245ac722daff3fd56d8a28
Shashank <mishra.shashankofficial06@gmail.com>
2022-11-06 22:29:01 +0530

    Added: PayRoll Service Program logic.

---
+++ src/com/day27/assignment/EmployeePayrollService.java
@@ -0,0 +1,13 @@
+package com.day27.assignment;
+
+public class EmployeePayrollService {
+
+    public enum IOStream {
+        CONSOLE_IO, FILE_IO, DB_IO
+    }
+
+    public static void main(String[] args) {
+        IOStream iOStream = IOStream.CONSOLE_IO;
+
+    }
+}
